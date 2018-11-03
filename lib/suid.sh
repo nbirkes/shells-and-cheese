099978f574b647967d562fe6f1c881940ec1fd92
Nathan Birkes <nathan.birkes@veracity.vision>
2018-11-03 16:00:24 -0500

    Using suid on a txt file made no sense

--- lib/suid.sh
+++ lib/suid.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-rm -f suid.txt
-touch suid.txt
-ls -l suid.txt
-chmod 4750 suid.txt
-ls -l suid.txt
-rm -f suid.txt
+rm -f bigprogram.sh
+touch bigprogram.sh
+ls -l bigprogram.sh
+chmod 6750 bigprogram.sh
+ls -l bigprogram.sh
+rm -f bigprogram.sh
 
